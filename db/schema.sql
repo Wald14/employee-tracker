8e5b46d4ce31fbd62c51eae89d1091fb8d5db355
Wald14 <lukewald@hotmail.com>
2023-11-02 10:02:23 -0500

    Added code to schema.sql to make database and create tables

--- db/schema.sql
+++ db/schema.sql
@@ -1,14 +1,31 @@
--- TODO: Create database ("employee_tracker_db")
+-- DONE: Create database ("employee_tracker_db")
+DROP DATABASE IF EXISTS employee_tracker_db;
+CREATE DATABASE employee_tracker_db;
+
+USE employee_tracker_db;
 
 -- TODO: Create department database with:
   -- id INT PRIMARY KEY
   -- department_name VARCHAR(30)
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL
+);
 
 -- TODO: Create role database with:
   -- id INT PRIMARY KEY
   -- title VARCHAR(30)
   -- salary DECIMAL
   -- department_id INT (FOREIGN KEY)
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT,
+  FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+);
 
 -- TODO: Create employee database with:
   -- id INT PRIMARY KEY
@@ -16,3 +33,16 @@
   -- last_name VARCHAR(30)
   -- role_id (FOREIGN KEY)
   -- manager_id ('FOREIGN' KEY but is the employee_id of the manager)
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id INT,
+  manager_id INT,
+  FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON DELETE SET NULL,
+  FOREIGN KEY (employee_id)
+    REFERENCES employee(id)
+    ON DELETE SET NULL
+);
