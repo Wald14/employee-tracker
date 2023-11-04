9780d0e97b0d9236609bd2aaffdd4150e187e3f0
Wald14 <lukewald@hotmail.com>
2023-11-04 14:11:03 -0500

    Added functionality to add an employee

--- db/add_queries.js
+++ db/add_queries.js
@@ -86,9 +86,11 @@ async function addRole() {
     // Clean up responses
     const newRole = res.newRole.trim();
     const newSal = res.newSalary.trim();
+
     // Grab name of department selected for console.log response
     const departmentName = (departmentList.find(departmentList => departmentList.value === res.pickedDepartment)).name
     console.log(departmentName)
+
     // Makes sure the role doesn't already exist
     const roleIndex = roleList.findIndex( roleList => roleList.title.toLowerCase() === res.newRole.toLowerCase())
     if (roleIndex >= 0) {
@@ -96,9 +98,11 @@ async function addRole() {
     } else {
     // If new, adds new role
       db.query(`INSERT INTO role (title, salary, department_id) VALUES ('${res.newRole}', ${parseInt(newSal)}, ${res.pickedDepartment});`)
+
       // Grab name of department selected for console.log response
       const departmentName = (departmentList.find(departmentList => departmentList.value === res.pickedDepartment)).name
       console.log(departmentName)
+
       // Console log confirmation
       console.log(`\n${newRole} has been added as a new role into the ${departmentName} department with a salary of ${parseInt(newSal)}!\n`)
     }
@@ -106,4 +110,59 @@ async function addRole() {
   })
 }
 
-module.exports = { addDepartment, addRole };
+
+// Add a employee
+async function addEmployee(){
+  // Query for all roles for role prompt
+  let select = 'id as value, title as name'
+  let from = 'role'
+  const roleList = await returnTable(select, from)
+  // Query for all roles for role prompt
+  select = 'id as value, concat(first_name," ",last_name) as name'
+  from = 'employee'
+  let extension = 'WHERE manager_id is null'
+  const managerList = await returnTable(select, from, extension)
+
+  // Prompt user for info
+  const answer = await inquirer.prompt([
+    {
+      type: 'input',
+      message: "What is the employee's first name?",
+      name: 'firstName',
+      validate: notNullValidator
+    },
+    {
+      type: 'input',
+      message: "What is the employee's last name?",
+      name: 'lastName',
+      validate: notNullValidator
+    },
+    {
+      type: 'list',
+      message: "What is the employee's role?",
+      name: 'pickedRole',
+      choices: roleList
+    },
+    {
+      type: 'list',
+      message: "Who is the manager for this employee?",
+      name: 'pickedManager',
+      choices: managerList
+    }
+  ])
+  .then((res) => {
+    console.log(res)
+    db.query(`
+    INSERT INTO employee (first_name, last_name, role_id, manager_id) 
+    VALUES ('${res.firstName}', '${res.lastName}', ${res.pickedRole}, ${res.pickedManager});`)
+    return;
+  })
+}
+
+
+
+
+
+
+
+module.exports = { addDepartment, addRole, addEmployee };
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const db = require('./config/connection')
 const { printTable } = require('console-table-printer');
 
 const { returnTable, viewAllDepartments, viewAllRoles, viewAllEmployees } = require('./db/viewAll_queries')
-const { addDepartment, addRole } = require('./db/add_queries')
+const { addDepartment, addRole, addEmployee } = require('./db/add_queries')
 
 // Specify on which port the Express.js server will run
 const PORT = process.env.PORT || 3001;
@@ -48,6 +48,10 @@ const listOfOptions = [
         value: 'addRole'
       },
       {
+        name: 'Add an Employee',
+        value: 'addEmployee'
+      },
+      {
         name: 'Quit',
         value: 'quit'
       }
@@ -82,6 +86,10 @@ async function start() {
       const newRole = await addRole()
       break;
 
+    case 'addEmployee':
+      const newEmployee = await addEmployee()
+      break;
+
     case 'quit':
       process.exit()
   }
@@ -91,82 +99,19 @@ start();
 
 
 // DONE: VIEW ALL DEPARTMENTS
-//      DONE: Run query for the whole department SQL table
 
 // DONE: VIEW ALL ROLES
-//      DONE: Run query for the whole role SQL table
 
 // DONE:VIEW ALL EMPLOYEES
-//      DONE: Run query for the whole empoyee SQL table
 
+// DONE: ADD A DEPARTMENT
 
-// DONE: Add a department
-//      DONE: Prompt user for department name
-//      DONE: Run query for a list of all the department_names
-//      DONE: Compare user entry to department list to make sure it doesn't   already exist
-//      DONE: If it does exist, let user know and don't add
-//      DONE: If it doesn't exist, add it to the department database
+// DONE: ADD A ROLE
+//      TODO: Check if the role already exists in specific department vs just in general
 
+// DONE: Add an employee
 
-// TODO: Add a role
-// DONE: Prompt user for role name
-// TODO: Compare user entry to role_name list to make sure it doesn't already exist
-// TODO: If it does exist, console.log(`${user-entry} is already a department`)
-// TODO: If it doesn't exist, continue
-// DONE: Prompt user for salary
-// DONE: Present user with a list of departments to add to
-// DONE: Run query for a list of all the department_names
-//  DONE: assign user input to variables and enter that into SQL table
-// async function addRole() {
-//   let select = 'id as value, name'
-//   let from = 'department'
-//   const departmentOptions = await returnTable(select, from);
-
-//   inquirer.prompt([
-//     {
-//       type: 'input',
-//       message: 'What role would you like to add?',
-//       name: 'newRole',
-//     },
-//     {
-//       type: 'input: number',
-//       message: 'What is the salary of this role?',
-//       name: 'newSalary',
-//     },
-//     {
-//       type: 'list',
-//       message: 'What department would you like to add this role to?',
-//       name: 'pickedDepartment',
-//       choices: departmentOptions
-//     }
-//   ])
-//   .then((answers) => {
-//   console.log(answers)
-//   console.log(`title: ${answers.newRole}`)
-//   console.log(`salary: ${answers.newSalary}`)
-//   console.log(`department_id: ${answers.pickedDepartment}`)
-//   db.query(`
-//     INSERT INTO role (title, salary, department_id) 
-//     VALUES ('${answers.newRole}', ${parseInt(answers.newSalary)}, ${answers.pickedDepartment});
-//     `,
-//     function (err, res) {
-//       console.log(`${answers.newRole} with a salary of ${answers.newSalary} has been added to the ${answers.pickedDepartment} department`)
-//       start()
-//     })
-//   })
-// }
-
-
-// TODO: Add an employee
-// TODO: Prompt user for employee first name
-// TODO: Prompt user for employee last name
-// TODO: Present user with a list of rolls to pick from
-// TODO: Run query for list of all roles
-// TODO: Present user with a list of managers to pick from
-// TODO: Run query for list of all employees who have NULL as their manager
-// TODO: assign user input to variables and enter that into SQL table
-
-// TODO: Quit (ends the program)
+// DONE: Quit (ends the program)
 
 
 /*
