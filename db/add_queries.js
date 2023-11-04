61bda271520b0effe3ea9ba375b90541c7e10034
Wald14 <lukewald@hotmail.com>
2023-11-04 11:56:18 -0500

    Added validator to make sure the user enters at least one character

--- db/add_queries.js
+++ db/add_queries.js
@@ -7,6 +7,16 @@ const inquirer = require('inquirer');
 const { returnTable, viewAllDepartments, viewAllRoles, viewAllEmployees } = require('./viewAll_queries')
 
 
+// Validators for inquirer prompts
+const confirmNotNullValidator = async (input) => {
+  if (input.length === 0) {
+     return 'Please enter at least one character';
+  }
+  return true;
+};
+
+
+// Add a Department
 async function addDepartment() {
   const departmentList = await viewAllDepartments()
   const answer = await inquirer.prompt([
@@ -14,17 +24,21 @@ async function addDepartment() {
       type: 'input',
       message: 'What would you like to call the new Department?',
       name: 'name',
+      validate: confirmNotNullValidator
     }
   ])
   .then((res) => {
-    const departmentIndex = departmentList.findIndex(departmentList => departmentList.name === res.name)
+    // Makes sure the department doesn't already exist
+    const newDep = res.name.trim();
+    const departmentIndex = departmentList.findIndex(departmentList => departmentList.name.toLowerCase() === newDep.toLowerCase())
     if (departmentIndex >= 0) {
-      console.log(`\nThe ${res.name} department already exists\n`)
+      console.log(`\nThe ${departmentList[departmentIndex].name} department already exists\n`)
     } else {
-       db.query(`INSERT INTO department (name) Values ('${res.name}')`)
-       console.log(`\n${res.name} has been added as a new department!\n`)
+      // If new, adds new department
+       db.query(`INSERT INTO department (name) Values ('${newDep}')`)
+       console.log(`\n${newDep} has been added as a new department!\n`)
     }
-    return res;
+    return;
     })
 }
 
